f5698cf03ce0dad0ff732d46ffbc4f2f1b713376
GRIBONVALD Julien <julien.gribonvald@recia.fr>
2023-06-28 13:32:26 +0200

    Local login

--- WebContent/karuta/js/pages/login.js
+++ WebContent/karuta/js/pages/login.js
@@ -233,7 +233,7 @@ function displayKarutaLogin()
 		url : serverBCK_API+"/credential/login",
 		data: data,
 		success : function(data) {
-			constructKarutaLogin(true);
+			constructKarutaLogin(true && localLogin);
 		},
 		error : function(jqxhr,textStatus) {
 			if (jqxhr.status == '404') {
